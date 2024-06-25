da0a0653abafaafe0bb61ddbb9f7cfcc4e9b0814
phuongcx123 <phuonglun66666@gmail.com>
2024-06-25 14:53:39 +0700

    crud category

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Thêm Mới Danh Mục </title>
+</head>
+<body>
+ <h1>Thêm Mới Danh Mục</h1>
+ <form action="{{route('categories.store')}}" method="post">
+     @csrf
+     <label for="name">Name</label>
+     <input type="text" name="name" id="name">
+     <button type="submit">Save</button>
+ </form>
+</body>
+</html>
